2d912ff8cbceeb20a261a2923a18252f0518282c
Mohamed Ouchen <ouchenmohammed963@gmail.com>
2026-01-22 12:56:48 +0100

    style general

---
+++ src/components/Header/BtmHeader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BtmHeader() {
+    return (
+        <div>
+            <h2> hello moahmed ouchen in your first websit</h2>
+        </div>
+    )
+}
