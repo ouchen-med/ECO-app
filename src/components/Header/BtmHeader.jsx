c8d9f0b2e4080531efa4e7993fae0441235e1126
Mohamed Ouchen <ouchenmohammed963@gmail.com>
2026-01-22 21:57:02 +0100

    add TopHeader

--- src/components/Header/BtmHeader.jsx
+++ src/components/Header/BtmHeader.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 export default function BtmHeader() {
     return (
         <div>
-            <h2> hello moahmed ouchen in your first websit</h2>
         </div>
     )
 }
--- src/components/Header/TopHeader.jsx
+++ src/components/Header/TopHeader.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import './Header.css'
+import { Link } from 'react-router-dom'
+import logo from '../../assets/logo.png'
+import { FaSearch } from "react-icons/fa";
+import { IoMdHeartEmpty } from "react-icons/io";
+import { BsCartCheck } from "react-icons/bs";
 
 export default function TopHeader() {
     return (
-        <div>
-            Top
+        <div className='top_header'>
+            <div className='container'>
+                <Link className='logo' to="/"><img src={logo} alt='Logo' /></Link>
+                <form action="" className='search_box'>
+                    <input type="text" name='search' id='search' placeholder='search For Products' />
+                    <button type='submit'><FaSearch /></button>
+                </form>
+                <div className="header_icons">
+                    <div className="icon">
+                        <IoMdHeartEmpty />
+                        <span className='count'>0</span>
+                    </div>
+                    <div className="icon">
+                        <BsCartCheck />
+                        <span className='count'>0</span>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
